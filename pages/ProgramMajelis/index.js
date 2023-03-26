5ce2ff1724c83711f09f2ab146626f6855b96b38
Fazrilutama <utamafazril8@gmail.com>
2023-03-26 15:46:32 +0700

    update 1

--- pages/ProgramMajelis/index.js
+++ pages/ProgramMajelis/index.js
@@ -1,11 +1,12 @@
 import Navbar from '@/components/Navbar'
+import ProgramIbuibu from '@/components/ProgramIbuibu'
 import React from 'react'
 
 export default function Programmajelis() {
   return (
     <div>
         <Navbar />
-        <ProgramMajelis />
+        <ProgramIbuibu />
     </div>
   )
 }
